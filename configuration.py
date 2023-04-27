f02096b1d55deb737041b7fcd016f7942b5a84c5
seamus88 <a-semenov-88@yandex.ru>
2023-04-27 20:53:57 +0300

    my first (no) commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://aa044b7d-6fc0-4cc3-88ba-059aa62a6df5.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
---
+++ sender_stand_request.py
@@ -0,0 +1,25 @@
+import configuration
+import requests
+import data
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={"count": 20})
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставялем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
+                         headers=data.headers)
+
+response = post_new_user(data.user_body);
+print(response.status_code)
+print(response.json())
